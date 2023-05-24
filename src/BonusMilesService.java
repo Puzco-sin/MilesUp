577337da2f99f0c59d1c9c02022676f5f4fec633
Михаил Синёв <msinev@masterdm.ru>
2023-05-24 15:56:41 +0300

    fix

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,9 @@
 public class BonusMilesService {
-    public int  calculate(int cost) {
+    public int calculate(int cost) {
 
         int miles;
         int mCost = 20;
         miles = (cost / mCost);
-return miles;
+        return miles;
     }
 }
